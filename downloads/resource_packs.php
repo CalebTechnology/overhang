64a7e71df2f3b9ebcea03dcaa4fe332b99aa1a03
Caleb Eden <calebeden@Calebs-MacBook-Air.local>
2020-09-15 11:11:35 -0500

    Set up resource pack download page

--- downloads/resource_packs.php
+++ downloads/resource_packs.php
@@ -34,7 +34,45 @@
     <?php require('../includes/session.php') ?>
     <?php include('navbar_template.php')?>
     <main>
-
+      <div class="jumbotron">
+        <div class="container">
+          <h1 class="display-3">Behavior Packs</h1>
+          <p>Here is the page to download resource packs for personal use! Some are ports (direct or indirect) of the Java Edition <a href="https://www.vanillatweaks.net" target="_blank">Vanilla Tweaks</a>' Resource Packs, while others are my own creations!</p>
+          <p>For resoure packs, there is only the mcpack download (because it's just 1 pack, no behaviors bundled with it)</p>
+          <h5>Filter Packs: (coming soon)</h5>
+            <a class="btn btn-primary btn-lg" href="?tag=behavior_tweaks" role="button">Behavior Pack Variants</a>
+            <a class="btn btn-primary btn-lg" href="?tag=vanilla_tweaks" role="button">Vanilla Tweaks Ports</a>
+            <a class="btn btn-primary btn-lg" href="?tag=creative" role="button">Creative Mode Packs</a>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h1>All Resource Packs:</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <?php
+               include '../includes/resource_connect.php';
+               $sql = "SELECT * FROM packs ORDER BY pack_name";
+               foreach ($conn->query($sql) as $row) {
+                 $updated = date_create($row['updated']);
+                 $display_date = date_format($updated, "M d Y");
+                 echo "<div class='container'>" . PHP_EOL;
+                 echo "<h3 class='display-5'>" . $row['pack_name'] . "</h3>" . PHP_EOL;
+                 echo "<p>Tags: (coming soon)</p>" . PHP_EOL;
+                 echo "<h6>Version: " . $row['version'] . "&emsp;Updated: " . $display_date . "</h6>" . PHP_EOL;
+                 echo "<p>Image coming soon</p>" . PHP_EOL;
+                 echo "<p class='lead'>" . $row['description'] . "</p>" . PHP_EOL;
+                 echo"<a class='btn btn-primary btn-lg' href='resource_pack/" . $row['mcpack'] . "' role='button' download>Download Mcpack</a>" . PHP_EOL;
+                 echo "<hr></div>" . PHP_EOL;
+               }
+            ?>
+          </div>
+        </div>
+      </div>
+      <br><br><br><br>
     </main>
     <?php include('footer.php') ?>
   </body>
